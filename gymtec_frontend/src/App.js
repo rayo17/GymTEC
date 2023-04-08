aea6f034883ad3051288483c919c15225c37f21f
Ignacio Grane <ignaciograner@gmail.com>
2023-04-08 01:30:23 -0600

    Esqueleto frontend

--- gymtec_frontend/src/App.js
+++ gymtec_frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from "react";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import VistaPrincipal from './VistaPrincipal';
+import LoginAdmin from './VistaAdministrador/LoginAdmin';
+import LoginCliente from './VistaCliente/LoginCliente';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Routes>
+            <Route path='/' element={ <VistaPrincipal/> }/>
+            <Route path='/loginadmin' element={ <LoginAdmin/> }/>
+            <Route path='/logincliente' element={ <LoginCliente/> }/>
+          </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ gymtec_frontend/src/VistaCliente/LoginCliente.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function LoginCliente () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoginCliente
